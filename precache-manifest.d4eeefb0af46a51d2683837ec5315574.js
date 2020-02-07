c42ac7a295893fd59a047dbb6042ead82ab12f5e
Suren <surenspost@gmail.com>
2020-02-07 20:09:12 +0530

    Updates

---
+++ precache-manifest.d4eeefb0af46a51d2683837ec5315574.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68861ca36ebf5bcfad8549c3103ad96f",
+    "url": "/static-react-ui/index.html"
+  },
+  {
+    "revision": "036e36288be807d1edcf",
+    "url": "/static-react-ui/static/js/main.6529b1c2.chunk.js"
+  },
+  {
+    "revision": "2f1ac467c0147c2cabee",
+    "url": "/static-react-ui/static/js/runtime-main.b188bc30.js"
+  }
+]);
---
+++ static/js/main.6529b1c2.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpstatic-react-ui"]=this["webpackJsonpstatic-react-ui"]||[]).push([[0],[function(t,c,i){t.exports=i(1)},function(t,c){}],[[0,1]]]);
+//# sourceMappingURL=main.6529b1c2.chunk.js.map
